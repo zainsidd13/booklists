c05fed8cd7e9f258187869a40e7c7f2dfb8fc926
Zain Siddiqui <zainsidd13@hotmail.com>
2023-06-13 18:47:46 -0400

    Created landing template for ListPage

--- booklists-vite/bookweb-vite/src/pages/ListPage.jsx
+++ booklists-vite/bookweb-vite/src/pages/ListPage.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container } from 'react-bootstrap';
+import { Container, Stack } from 'react-bootstrap';
 
 function ListPage() {
     const { argument } = useParams();
@@ -41,9 +41,24 @@ function ListPage() {
     return (
         <Container fluid>
             <h1 className='title'>{list[0]?.list_title}</h1>
-            {books?.map((book) => (
-              <h1>{book.book_title}</h1>
+            <p className='d-flex justify-content-center'>{list[0].desc}</p>
+
+          <div className=''>
+            {books?.map((book, index) => (
+              <Stack gap={3} direction='horizontal' className='mt-5 d-flex justify-content-center'>
+                                   
+                <Stack gap={2} direction='horizontal' style={{display: 'flex'}}>
+                    <p style={{alignSelf: 'flex-start'}}>{index+1}.</p>
+                    <img src={book.book_cover} alt={book.book_title} />
+                    <div style={{alignSelf: 'flex-start'}}>
+                        <p style={{ fontWeight: 'bold', marginBottom: '1px' }} className=''>{book.book_title}</p>
+                        <p className='text-muted'>by {book.book_author}</p>
+                    </div>
+                </Stack>
+          
+               </Stack>
             ))}
+          </div>
         </Container>
     )
 }
